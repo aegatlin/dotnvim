72714adfa1c60fa326170a9abcef28306353bc97
Austin Gatlin <austin@gatlin.io>
2023-10-08 11:09:24 +1100

    Add multiple language packs and auto-save.nvim

---
+++ README.md
@@ -0,0 +1,14 @@
+# dotnvim
+
+This repo is embedded within the [AstroNvim](https://astronvim.com/) repo, which
+is my "neovim framework" of choice.
+
+## Setup
+
+Follow the [AstroNvim getting started guide](https://docs.astronvim.com/). Clone
+this repo at the point where you setup custom configurations.
+
+```sh
+git clone git@github.com:aegatlin/dotnvim.git ~/.config/nvim/lua/user
+```
+
--- init.lua
+++ init.lua
@@ -4,6 +4,23 @@ return {
   plugins = {
     "AstroNvim/community",
     { import = "astrocommunity.motion.leap-nvim" },
+    { import = "astrocommunity.editing-support.auto-save-nvim" },
+    -- to explore next
+    -- { import = "astrocommunity.split-and-window.windows-nvim" },
+
+    -- language packs
+    { import = "astrocommunity.pack.docker" },
+    { import = "astrocommunity.pack.json" },
+    { import = "astrocommunity.pack.lua" },
+    { import = "astrocommunity.pack.markdown" },
+    { import = "astrocommunity.pack.python" },
+    { import = "astrocommunity.pack.rust" },
+    { import = "astrocommunity.pack.tailwindcss" },
+    { import = "astrocommunity.pack.toml" },
+    { import = "astrocommunity.pack.typescript-all-in-one" },
+    { import = "astrocommunity.pack.yaml" },
+
+    -- themes
     {
       "catppuccin/nvim",
       name = "catppuccin",
